e44468d5d8efcd788c189cf70453b51bf8d6b25e
rhodso <rhodsoprivate@gmail.com>
2022-04-13 19:03:06 +0100

    Added crude word wrap, improving soon tm

--- index.php
+++ index.php
@@ -10,9 +10,13 @@
         $buttonWidth = 150;
         $buttonHeight = 150;
 
+        $buttonTextLength = 12;
+
         // ----------------
         // Do not change anything below this line
         $vertical = $topOffset + (5*($buttonHeight + $topPadding))
+
+        
     ?>    
 
     <head>
@@ -70,10 +74,28 @@
             $leftPos = $leftOffset;
             $buttonText = "Here's some text";
 
-
-
             for ($i = 1; $i <= 25; $i++) {
                 $buttonText = $lines[$i];
+
+                // If length of $buttonText is greater than 12 characters, then do the split
+                if(strlen($buttonText) > $buttonTextLength){
+
+                    // Split into array of words
+                    $buttonTextArray = explode(" ", $buttonText);
+                    
+                    // Add a line break every 2 words
+                    for($j = 0; $j <= count($buttonTextArray); $j+=2){
+                        $buttonTextArray[$j] .= "<br>";
+                    }
+
+                    // var_dump($buttonTextArray);                
+
+                    // Recombine text into string
+                    $buttonText = "";
+                    for($j = 0; $j <= count($buttonTextArray); $j++){
+                        $buttonText .= $buttonTextArray[$j] . " ";
+                    }
+                }
 
                 echo "<button onclick=\"changeColor(this.id, this.style.background)\" id=\"btn" . $i . 
                 "\" class=\"button\" style=\"white-space: normal !important; word-break: break-word; text-align: center; background: rgb(255, 255, 255); left: ". $leftPos. 
