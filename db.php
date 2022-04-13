07a4a1571f3f8b7b0024b9747b3f12296fc076f4
rhodso <rhodsoprivate@gmail.com>
2022-04-13 20:20:33 +0100

    Cleaned up db.php

--- db.php
+++ db.php
@@ -3,6 +3,8 @@
     
     function sqlSafeFormVal($str){
         $ans = trim($str);
+        $ans = str_replace("\"", "", $ans);
+        $ans = str_replace("'", "", $ans);
         return $str;
     }
 
@@ -10,68 +12,69 @@
         //  Usnig global DB
         global $db;
 
-        //SQL statement
+        // SQL statement
         $sql = "SELECT Bingo_Name FROM \"bingoGrids\";";
 
-        //Execute query and store results in new array res
+        // Execute query and store results in new array res
         $results = $db->query(sqlSafeFormVal($sql));
         $res = array();
         while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
             array_push($res, $row);
         }
 
-        //Extract the data we want
+        // Extract the data we want
         $bingoGrids = array();
         for($i = 0; $i < count($res); $i++){
             array_push($bingoGrids, $res[$i]["Bingo_Name"]);
         }
 
-        //Return the data
+        // Return the data
         return $bingoGrids;
     }
 
     function getLinesFromBingo($bingoID){
-        //  Usnig global DB
+        // Using global DB
         global $db;
 
-        //SQL statement
-        $sql = "SELECT Line_Name FROM \"bingoLines\" WHERE Bingo_ID = " . $bingoID .";";
+        // SQL statement
+        $sql = "SELECT Line_Name FROM \"bingoLines\" WHERE Bingo_ID = " . sqlSafeFormVal($bingoID) .";";
 
         // Execute query and store results in new array res
-        $results = $db->query(sqlSafeFormVal($sql));
+        $results = $db->query($sql);
 
         $res = array();
         while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
             array_push($res, $row);
         }
 
-        //Extract the data we want
+        // Extract the data we want
         $bingoLines = array();
         for($i = 0; $i < count($res); $i++){
             array_push($bingoLines, $res[$i]["Line_Name"]);
         }
 
-        //Shuffle the array
+        // Shuffle the array
         shuffle($bingoLines);
 
-        //Return the data
+        // Return the data
         return $bingoLines;
     }
 
     function getIDFromName($bingoName){
-        //  Usnig global DB
+        // Using global DB
         global $db;
 
-        //SQL statement
+        // SQL statement
         $sql = "SELECT bingoGrids.Bingo_ID FROM bingoGrids WHERE bingoGrids.Bingo_Name = '" . sqlSafeFormVal($bingoName) ."'";
 
-        //Execute query and store results in new array res
-        $results = $db->query(sqlSafeFormVal($sql));
+        // Execute query and store results in new array res
+        $results = $db->query($sql);
         $res = array();
         while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
             array_push($res, $row);
         }
 
+        // Return -1 if not found, else return answer
         if(count($res) == 0){ return -1; }
         return $res[0]["Bingo_ID"];
     }   
