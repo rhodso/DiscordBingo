7adebcb3e361c8e3a611dd2dfe7c6ed54dbbd2a2
rhodso <rhodsoprivate@gmail.com>
2022-04-14 23:36:00 +0100

    It is done Bingo Beta.io.gov.edu.xyz is ready (not actual url)

--- db.php
+++ db.php
@@ -32,6 +32,33 @@
         return $bingoGrids;
     }
 
+    function getBingoGridInfo(){
+        //  Usnig global DB
+        global $db;
+
+        // SQL statement
+        $sql = "SELECT * FROM \"bingoGrids\";";
+
+        // Execute query and store results in new array res
+        $results = $db->query(sqlSafeFormVal($sql));
+        $res = array();
+        while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
+            array_push($res, $row);
+        }
+
+        // Extract the data we want
+        $bingoGrids = array(array());
+        for($i = 0; $i < count($res); $i++){
+            $tmp = array();
+            array_push($tmp, $res[$i]["Bingo_ID"]);
+            array_push($tmp, $res[$i]["Bingo_Name"]);
+            array_push($bingoGrids, $tmp);
+        }
+
+        // Return the data
+        return $bingoGrids;
+    }
+
     function getLinesFromBingo($bingoID){
         // Using global DB
         global $db;
@@ -58,6 +85,23 @@
 
         // Return the data
         return $bingoLines;
+    }
+
+    function checkValidBingoId($bingoID){
+        // Using global DB
+        global $db;
+
+        // SQL
+        $sql = "SELECT bingoGrids.Bingo_ID FROM bingoGrids WHERE bingoGrids.Bingo_ID = " . sqlSafeFormVal($bingoID);
+
+        $results = $db->query($sql);
+        $res = array();
+        while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
+            array_push($res, $row);
+        }
+
+        //Return if a result was found or not
+        return count($res) > 0;
     }
 
     function getIDFromName($bingoName){
--- index.php
+++ index.php
@@ -14,7 +14,8 @@
 
         // ----------------
         // Do not change anything below this line
-        $vertical = $topOffset + (5*($buttonHeight + $topPadding))
+        $vertical = $topOffset + (5*($buttonHeight + $topPadding));
+        $showBingo = false;
     ?>    
 
     <head>
@@ -34,47 +35,82 @@
         <!-- Get lines from bingo DB -->
         <?php
             include "db.php";
+
+            //some testing stuff
             $grids = getBingoGrids();
             $lines = getLinesFromBingo(3);
-
             // $id = getIDFromName("TestList");
-        ?>
-        <label for="BingoGrids">Choose a bingo grid to play: </label>
-        <select name="BingoGrids" id="bingoGridSelection" class="select" style="width: 587px">bingoGridSelection
-            <option>
-            <?php
-                for($i = 0; $i < count($grids); $i++){
-                    echo "<option>" . $grids[$i] ."</option>";
+
+            // Setup new grids for later
+            $newGrids = getBingoGridInfo();
+            
+            // Parse URL argument
+            $url_components = parse_url($_SERVER['REQUEST_URI']);
+            parse_str($url_components['query'], $params);
+
+            // Check if we actually have any params
+            if(count($params) > 0){
+                // If the params are valid
+                if(checkValidBingoId($params['bingo_id'])){
+                    // Param is valid, so get the lines and show the bingo
+                    $lines = getLinesFromBingo($params['bingo_id']);
+                    $showBingo = true;
+                } else {
+                    // Invalid bingo
+                    echo "<h1 style=\"color: red\">Invalid Bingo ID</h1>";    
                 }
-            ?>
-        </select>
+            }
+
+            //Ensure that we have enough lines
+            if(count($lines) < 25){
+                $showBingo = false;
+            }
+        ?>
+        <script>
+            var bingoContent = <?php echo json_encode($newGrids); ?>;
+            // console.log(bingoContent);
+        </script>
+        <div>
+            <label for="BingoGrids">Choose a bingo grid to play: </label>
+            <select name="BingoGrids" id="bingoGridSelection" class="select" style="width: 500">bingoGridSelection
+                <option>
+                <?php
+                    for($i = 0; $i < count($grids); $i++){
+                        echo "<option>" . $grids[$i] ."</option>";
+                    }
+                ?>
+            </select>
+            <button id="changeBingoButton" onclick="changeBingo()" style="position: absolute; left: 700px;">Play bingo</button>
+        </div>
         <div>
         <!-- Add some buttons -->
         <?php
-            // Setup controls
-            $topPos = $topOffset;
-            $leftPos = $leftOffset;
-            $buttonText = "Here's some text";
-            
-            // Add each button
-            for ($i = 1; $i <= 25; $i++) {
-                // Get the word text
-                $buttonText = wordwrap($lines[$i],$buttonTextLength, "<br>");
-                if($i == 13){ $buttonText = "Free space"; }
+            if($showBingo){           
+                // Setup controls
+                $topPos = $topOffset;
+                $leftPos = $leftOffset;
+                $buttonText = "Here's some text";
                 
-                // Echo out the actual HTML button
-                echo "<button onclick=\"changeColor(this.id, this.style.background)\" id=\"btn" . $i . 
-                "\" class=\"button\" style=\"background: rgb(255, 255, 255);left: ". $leftPos. 
-                "; top: ". $topPos . "\"><p>" . $buttonText . "</p></button>";
+                // Add each button
+                for ($i = 1; $i <= 25; $i++) {
+                    // Get the word text
+                    $buttonText = wordwrap($lines[$i],$buttonTextLength, "<br>");
+                    if($i == 13){ $buttonText = "Free space"; }
+                    
+                    // Echo out the actual HTML button
+                    echo "<button onclick=\"changeColor(this.id, this.style.background)\" id=\"btn" . $i . 
+                    "\" class=\"button\" style=\"background: rgb(255, 255, 255);left: ". $leftPos. 
+                    "; top: ". $topPos . "\"><p>" . $buttonText . "</p></button>";
 
-                // Increment left pos
-                $leftPos += $leftPadding + $buttonWidth;
+                    // Increment left pos
+                    $leftPos += $leftPadding + $buttonWidth;
 
-                // "Carriage return" for button placement
-                if($i % 5 == 0){ 
-                    echo "<br>";
-                    $leftPos = $leftOffset;
-                    $topPos += $topPadding + $buttonHeight;
+                    // "Carriage return" for button placement
+                    if($i % 5 == 0){ 
+                        echo "<br>";
+                        $leftPos = $leftOffset;
+                        $topPos += $topPadding + $buttonHeight;
+                    }
                 }
             }
         ?>
@@ -96,6 +132,24 @@
             else{ // Catch all
                 document.getElementById(id).style.background='#ffffff';
                 document.getElementById(id).style.color='black';
+            }
+        }
+        function changeBingo(){
+            //Get the dropdown selected value
+            var select = document.getElementById('bingoGridSelection')
+            var val = select.options[select.selectedIndex].value;
+
+            //Look up the ID
+            var match = -1;
+            for(let i=0; i < bingoContent.length; i++){
+                if(val == bingoContent[i][1]){
+                    match = bingoContent[i][0];
+                }
+            }
+            
+            //Go to the corresponding page
+            if(match > 0){
+                window.location.assign("index.php?bingo_id=" + match); 
             }
         }
         </script>
